a87e7d3295ce49f039f813fe5411bef77146ad6e
Neosoulink <neodarksoulink@gmail.com>
2021-11-20 16:38:25 +0100

    Narrowing part 1

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,16 @@ import "./style.css";
 //const app = document.querySelector<HTMLDivElement>('#app')!
 //console.log(app);
 
-// The primitives types
-let str: string = "ds";
-let num: number = 0;
-let bool: boolean = true;
+// Narrowing
 
-console.log("The primitives: \n", str, num, bool);
-
-// Arrays types
-let strArr: string[] = ["1", "2"];
-let numArr: Array<number> = [1, 2];
-
-console.log("\nArrays types: \n", strArr, numArr);
-
-// ANY
-let something: any = { x: 0 };
-something.foo;
-something.bar = () => {};
+function printAll(strs: string | string[] | null) {
+	if (typeof strs === "object") {
+		for (const s of strs) {
+			console.log(s);
+		}
+	} else if (typeof strs === "string") {
+		console.log(strs);
+	} else {
+		// do nothing
+	}
+}
