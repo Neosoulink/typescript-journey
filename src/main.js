f725b65bfe012a7e4925c9b0a2ed6dc7f021b04b
Neosoulink <neodarksoulink@gmail.com>
2021-11-20 15:58:16 +0100

    The basics

--- src/main.js
+++ src/main.js
@@ -1,41 +1,30 @@
-import './style.css'
-
+"use strict";
+exports.__esModule = true;
+require("./style.css");
 //const app = document.querySelector<HTMLDivElement>('#app')!
 //console.log(app);
-
-
 // STATIC CHECKING
 //const message ="Some text";
 //console.log(message()));
-
-
 // Non-exception Failures
 //const user = {
 //  name: "Daniel",
 //  age: 26,
 //};
 //user.location;
-
-
 //const announcement = "Hello World!";
-
 //// How quickly can you spot the typos?
 //announcement.toLocaleLowerCase();
 //announcement.toLocaleLowerCase();
-
 //// We probably meant to write this...
 //announcement.toLocaleLowerCase();
-
 //function flipCoin() {
 //  // Meant to be Math.random()
 //  return Math.random() < 0.5;
 //}
-
 //const value = Math.random() < 0.5 ? "a" : "b";
 //if (value !== "a") {
 //  // ...
 //} else if (value === "b") {
-
 //}
-
 // Types for Tooling
